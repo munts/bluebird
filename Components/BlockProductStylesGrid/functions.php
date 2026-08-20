706584ef98a81e2cc5690dc7ff621deb3486d187
Scott Taylor <s.taylor06@gmail.com>
2026-08-20 15:56:36 -0600

    BlockProductStylesGrid html id attribute etc

--- Components/BlockProductStylesGrid/functions.php
+++ Components/BlockProductStylesGrid/functions.php
@@ -2,6 +2,13 @@
 
 namespace Flynt\Components\BlockProductStylesGrid;
 
+add_filter('Flynt/addComponentData?name=BlockProductStylesGrid', function (array $data): array {
+    if (!empty($data['htmlId'])) {
+        $data['htmlId'] = sanitize_title($data['htmlId']);
+    }
+    return $data;
+});
+
 function getACFLayout(): array
 {
     return [
@@ -12,6 +19,13 @@ function getACFLayout(): array
                 'label' => __('Heading', 'flynt'),
                 'instructions' => __('e.g. "Find the Perfect Window Style". Add one instance of this block per product line (Windows, Doors, Siding) to group them.', 'flynt'),
                 'name' => 'heading',
+                'type' => 'text',
+                'required' => 0,
+            ],
+            [
+                'label' => __('HTML ID / Anchor', 'flynt'),
+                'instructions' => __('Optional. Lets you link directly to this specific grid from elsewhere on the site, e.g. "windows-styles" → yoursite.com/page/#windows-styles. Since you can have multiple instances of this block on a page (Windows, Doors, Siding), give each a distinct value if you plan to link to more than one.', 'flynt'),
+                'name' => 'htmlId',
                 'type' => 'text',
                 'required' => 0,
             ],
